22c6e71bda48c101e3575e89e615fa536ad6a757
chooo1234 <77899134+chooo1234@users.noreply.github.com>
2022-06-09 04:23:30 -0500

    Elminar key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyCfEY5HBZcL3Z4ppOfaEEJR57mRw22Qc-E",
-    authDomain: "miaplicaciondelascosas.firebaseapp.com",
-    databaseURL: "https://miaplicaciondelascosas-default-rtdb.firebaseio.com",
-    projectId: "miaplicaciondelascosas",
-    storageBucket: "miaplicaciondelascosas.appspot.com",
-    messagingSenderId: "188759710557",
-    appId: "1:188759710557:web:df1b0b26e14cf7d4b566b4"
-  }
+
 };
 
 /*
